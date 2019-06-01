45cc77cfedae742c6ecd516b739ee28d64e8f643
Ellmanc <celia.ellmann@free.fr>
2019-06-01 14:33:59 +0200

    e

--- src/location/ListeLocations.java
+++ src/location/ListeLocations.java
@@ -1,8 +1,12 @@
-package location;
+package client;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class ListeLocations extends ArrayList<Location> implements Serializable
+/** Intermédiaire pour sérialiser et enregistrer des clients.
+ * @author Dejan Paris
+ */
+public class ListeLocations extends ArrayList<Client> implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 }
